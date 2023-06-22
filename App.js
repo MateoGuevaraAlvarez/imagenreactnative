cc39b7a849fc6927a1b6bd744d37184d48088ca6
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-06-22 12:03:54 -0300

    card

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
   import { StatusBar } from 'expo-status-bar';
   import { StyleSheet, Text, View, Image } from 'react-native';
-  import { Card } from './components/card'
+  import { Cardcomp } from './components/card'
 
   export default function App() {
     return (
       <View style={styles.container}>
-        <Image source={require('./assets/Image2.jpeg')} style={{ width: "50%", height: "30%" }} />
-        <Card></Card>
+        <Cardcomp/>
         <StatusBar style="auto" />
       </View>
     );
--- components/card.jsx
+++ components/card.jsx
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, Button, Pressable} from 'react-native';
-import {Card} from 'react-native-elements'
+import React from 'react';
+import { View, Text,Image} from 'react-native';
 
-export const card = () => {
-    return (
-        <Card image= {require('../assets/image.jpeg')}>
-            <Text style={{marginBottom:10}}> lolass</Text>
-        </Card>
-    );
+
+export const Cardcomp = () => {
+        return (
+            <View style={{ backgroundColor: '#fff',borderRadius: 10,padding: 16,margin: 8,shadowColor: '#000',shadowOpacity: 0.2,shadowOffset: { width: 0, height: 2 },elevation: 2,}}>
+                <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 8 }}>
+                    <Image source={require('./assets/coffe1.jpeg')} style={{ width: 32, height: 32, borderRadius: 16, marginRight: 8 }}/>
+                    <Text style={{ fontSize: 20 }}>Card Title</Text>
+                </View>
+                <Text style={{ marginBottom: 8 }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse imperdiet consectetur risus.</Text>
+            </View>
+        );
 }
 
-export default card;
+export default Cardcomp;
